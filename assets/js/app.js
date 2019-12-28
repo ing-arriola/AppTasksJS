29f8dc0a2f4834215a88f249958b2aff0dcf3ce4
ing-arriola <ingeniero.arriola@gmail.com>
2019-12-28 11:17:20 -0600

    Add a link to delete the task

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,9 +18,15 @@ function addTask(e){
     //Well the message works.. then lets get the message from the text-area task
     const task=document.getElementById('task').value
 
+    //Add a link to delete the task
+    const deleteLink=document.createElement('a')
+    deleteLink.classList='delete-task'//Add a class to style the link
+    deleteLink.innerText='X'
+
     //Creating a new li element to list the task in the HTML
     const li=document.createElement('li')
     li.innerText=task//Add the text of the task to the li
+    li.appendChild(deleteLink)//Add the child for every task... i.e. when you add a task you will be able to delete them one by one
     TaskList.appendChild(li)//Add the new child element
 
     console.log(task)
