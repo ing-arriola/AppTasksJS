02dbeda5b82955eef868925a58b40f57958ffdd6
ing-arriola <ingeniero.arriola@gmail.com>
2019-12-28 11:08:02 -0600

    Success at sending the task from the form to DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,5 +18,10 @@ function addTask(e){
     //Well the message works.. then lets get the message from the text-area task
     const task=document.getElementById('task').value
 
+    //Creating a new li element to list the task in the HTML
+    const li=document.createElement('li')
+    li.innerText=task//Add the text of the task to the li
+    TaskList.appendChild(li)//Add the new child element
+
     console.log(task)
 }
